1c1667a16e9fd096eece47d64b4d3432f5005e93
Gyan <gyan@intectum.nz>
2024-08-06 15:24:02 +0100

    Added links and minor format changes to home page

--- apps/intectum-website/src/pages/Home.tsx
+++ apps/intectum-website/src/pages/Home.tsx
@@ -28,70 +28,96 @@ const Freelance: FC = () =>
           <h1><span className="u-color--primary">freelance</span> senior developer</h1>
           <h2 className="u-color--primary">Hello! My name is Gyan (aka Gary Buyn)</h2>
           <div className="u-text-center">
+            <a href="https://www.linkedin.com/in/intectum" target="_blank" rel="noreferrer" className="c-button c-button--primary u-m--sm">LinkedIn</a>
             <a href={cvUrl} target="_blank" rel="noreferrer" className="c-button c-button--primary u-m--sm">Full CV</a>
+            <a href="https://github.com/gyan-intectum-nz" target="_blank" rel="noreferrer" className="c-button c-button--primary u-m--sm">GitHub</a>
             <a href="mailto:gyan@intectum.nz" className="c-button c-button--primary u-m--sm">gyan@intectum.nz</a>
           </div>
         </section>
-        <div className="u-p" />
-        <section className="o-columns-3 u-my--lg">
-          <div className="u-m">
-            <div className="u-text-center">
-              <img src="/images/code.png" alt="Code" />
+        <div className="u-p"/>
+        <section className="u-my--lg">
+          <div className="o-columns-3">
+            <div className="u-mx u-text-center">
+              <img src="/images/code.png" alt="Code"/>
+              <h3 className="u-color--primary">I have a passion for high quality code that is as simple as
+                possible.</h3>
             </div>
-            <h3 className="u-color--primary">I have a passion for high quality code that is as simple as possible.</h3>
-            <p className="u-large">In my experience the number one cause of bugs and hard to maintain software is complexity. Writing simpler code that does the same job makes it easier for other developers to understand, reduces the chance for bugs, makes it easier to change later on and often even improves performance.</p>
+            <div className="u-mx u-text-center">
+              <img src="/images/learn.png" alt="Learn"/>
+              <h3 className="u-color--primary">I learn new tech and concepts very quickly.</h3>
+            </div>
+            <div className="u-mx u-text-center">
+              <img src="/images/details.png" alt="Details"/>
+              <h3 className="u-color--primary">I pay a lot of attention to the details.</h3>
+            </div>
           </div>
-          <div className="u-m">
-            <div className="u-text-center">
-              <img src="/images/learn.png" alt="Learn" />
-            </div>
-            <h3 className="u-color--primary">I learn new tech and concepts very quickly.</h3>
-            <p className="u-large">I often jump into existing code-bases which almost inevitably use something that I have no previous experience with. I approach the new code-base task by task, determining how much needs to be learned to effectively complete the task at hand without attempting to learn everything. This means that I can join a project and be effective straight away.</p>
-          </div>
-          <div className="u-m">
-            <div className="u-text-center">
-              <img src="/images/details.png" alt="Details" />
-            </div>
-            <h3 className="u-color--primary">I pay a lot of attention to the details.</h3>
-            <p className="u-large">Although I do not attempt to learn everything before performing a single task in a project, I will learn what I need to know to perform that task to a high standard. Not just to get it done but to get it done well. I always try to find the canonical method and use the strengths of the project, languages and technologies. I will also look for weaknesses in the project and suggest improvements.</p>
+          <div className="o-columns-3">
+            <p className="u-mx u-my--none u-large">
+              In my experience the number one cause of bugs and hard to maintain software is
+              complexity. Writing simpler code that does the same job makes it easier for other developers to
+              understand, reduces the chance for bugs, makes it easier to change later on and often even improves
+              performance.
+            </p>
+            <p className="u-mx u-my--none u-large">
+              I often jump into existing code-bases which almost inevitably use something that I
+              have no previous experience with. I approach the new code-base task by task, determining how much needs
+              to be learned to effectively complete the task at hand without attempting to learn everything. This
+              means that I can join a project and be effective straight away.
+            </p>
+            <p className="u-mx u-my--none u-large">
+              Although I do not attempt to learn everything before performing a single task in a
+              project, I will learn what I need to know to perform that task to a high standard. Not just to get it
+              done but to get it done well. I always try to find the canonical method and use the strengths of the
+              project, languages and technologies. I will also look for weaknesses in the project and suggest
+              improvements.
+            </p>
           </div>
         </section>
-        <div className="u-p" />
+        <div className="u-p"/>
         <section className="u-my--lg">
           <h2>Experience</h2>
           <div className="o-grid">
             {experienceTags?.map(tag =>
               <Link key={tag.slug} className="u-text-center u-m" to={`/projects?tag=${tag.slug}`}>
-                <img className="c-icon" src={tag.iconUrl} alt={tag.name} />
+                <img className="c-icon" src={tag.iconUrl} alt={tag.name}/>
                 <h4>{tag.name}</h4>
               </Link>
             )}
           </div>
         </section>
-        <div className="u-p" />
+        <div className="u-p"/>
+        <section className="u-my--lg">
+          <h2>Vetted Engineer at</h2>
+          <div className="o-grid">
+            <Link className="u-text-center u-m" to="">
+              <img className="c-icon" src="images/arc-dev.svg" alt="arc()"/>
+            </Link>
+          </div>
+        </section>
+        <div className="u-p"/>
         <section className="u-my--lg">
           <h2>Clients</h2>
           <div className="o-grid">
             {activeClients?.map(client =>
               <button key={client.slug} className="c-button" onClick={() => setClient(client)}>
-                <img className="c-icon" src={client.iconUrl} alt={client.name} />
+                <img className="c-icon" src={client.iconUrl} alt={client.name}/>
               </button>
             )}
           </div>
         </section>
-        <div className="u-p" />
+        <div className="u-p"/>
         <section className="u-my--lg">
           <h2>Tools of the Trade</h2>
           <div className="o-grid">
             {toolTags?.map(tag =>
               <Link key={tag.slug} className="u-m" to={`/projects?tag=${tag.slug}`}>
-                <img className="icon" src={tag.iconUrl} alt={tag.name} />
+                <img className="icon" src={tag.iconUrl} alt={tag.name}/>
               </Link>
             )}
           </div>
         </section>
       </div>
-      {client && <ClientModal client={client} onDismiss={() => setClient(undefined)} />}
+      {client && <ClientModal client={client} onDismiss={() => setClient(undefined)}/>}
     </>
   );
 };

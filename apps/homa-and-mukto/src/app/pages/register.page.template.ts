e2b9121c0919a05a47e0d18eacbf1eea63b0d512
Gyan <gyan@intectum.io>
2026-07-07 11:51:57 +0100

    Prevent horizontal cropping on iPhone

--- apps/homa-and-mukto/src/app/pages/register.page.template.ts
+++ apps/homa-and-mukto/src/app/pages/register.page.template.ts
@@ -1,7 +1,7 @@
 const renderPageHTML = () => `
   <main class="u-container u-cover-screen u-fc u-center u-my--lg u-gap--lg">
     <img src="/images/logo-vertical.png" alt="Homa & Mukto" style="width: 200px; align-self: center;" />
-    <div data-init="register" style="max-width: 600px;"></div>
+    <div data-init="register" style="width: 100%; max-width: 600px;"></div>
   </main>
 `;
 
